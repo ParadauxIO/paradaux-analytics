ca0d5af852916671c6df017b3ae737ee3d4518c2
Rían Errity <rian@paradaux.io>
2023-05-18 20:15:15 +0200

    Remove config import

--- supabase/functions/analytics/index.ts
+++ supabase/functions/analytics/index.ts
@@ -4,7 +4,6 @@
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import config from "./config.json" assert { type: "json" };
 import { corsHeaders } from '../_shared/cors.ts'
 const supabase = createClient(Deno.env.get("SUPABASE_URL"), Deno.env.get("SUPABASE_SERVICE_ROLE_KEY"));
 
